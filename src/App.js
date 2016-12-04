424ea426ce6e9ed840f270b8446cfcf374f00c6a
Ben Bruscella <benbruscella@gmail.com>
2016-12-04 15:38:37 +1100

    3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
-// class App extends React.Component {
-//   render () {
-//     return <h1>Hello World</h1>
-//   }
-// }
+class App extends React.Component {
+  render () {
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <b>bold</b>
+      </div>
+    )
+  }
+}
 
-const App = () => <h1>Hello stateless</h1>
+// const App = () => <h1>Hello stateless</h1>
 
 export default App
